c95acd51825003401c35e38aa903f736f228462b
lanxeon <bswapnonil@yahoo.com>
2021-06-06 17:14:52 +0530

    got load forwarding to kind of work

--- fog.js
+++ fog.js
@@ -1,6 +1,15 @@
+const fs = require("fs");
+
 module.exports = function (RED) {
   function Fog(config) {
     RED.nodes.createNode(this, config);
+
+    fs.writeFileSync("lmao.txt", "hello");
+
+    // timeout function
+    function timeout(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 
     // initializing properties
     this.number = +config.number;
@@ -17,17 +26,25 @@ module.exports = function (RED) {
     nodeContext.set("capacity", node.capacity);
     nodeContext.set("jobs", []);
 
-    function timeout(ms) {
-      return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-
     let fogNodes = flowContext.get("fogNodes");
-    if (fogNodes) {
+    let highestLevel = flowContext.get("highestLevel");
+
+    // for setting highest level among fog nodes
+    if (!highestLevel || this.level > highestLevel)
+      flowContext.set("highestLevel", this.level);
+
+    // for setting up the global state of fog nodes by levels and then node number
+    if (!fogNodes) {
       flowContext.set("fogNodes", {
-        [node.number]: { ...node },
+        [this.level]: { [node.number]: { ...node } },
       });
     } else {
-      flowContext.set("fogNodes", { ...fogNodes, [node.number]: { ...node } });
+      flowContext.set("fogNodes", {
+        ...fogNodes,
+        [this.level]: fogNodes[this.level]
+          ? { ...fogNodes[this.level], [node.number]: { ...node } }
+          : { [node.number]: { ...node } },
+      });
     }
 
     // when flow reaches here with a msg
@@ -35,7 +52,51 @@ module.exports = function (RED) {
       // wait for this before performing actions
       await timeout(node.latency);
 
-      node.send(msg);
+      // perform actions
+      let { payload } = msg;
+
+      if (payload.capacity > nodeContext.get("capacity")) {
+        if (this.level === flowContext.get("highestLevel")) {
+          // go to cloud
+        }
+
+        let fogNodes = node.context().flow.get("fogNodes");
+        let nextLevel = fogNodes[node.level + 1];
+
+        let highestCap = 0,
+          highestCapNode;
+        for (const levelNode of Object.keys(nextLevel)) {
+          if (nextLevel[levelNode].capacity > highestCap)
+            highestCap = nextLevel[levelNode].capacity;
+          highestCapNode = levelNode;
+        }
+
+        node.send({
+          ...payload,
+          forwardTo: highestCapNode,
+          forwaredBy: node.number,
+        });
+      }
+
+      // 1000 ips
+      // 300 instructions, 45
+
+      let subtractedCapacity = payload.capacity;
+      let curCapacity = nodeContext.get("capacity");
+      let newCapacity = curCapacity - subtractedCapacity;
+      nodeContext.set("capacity", newCapacity);
+
+      let curFogNodes = node.context().flow.get("fogNodes");
+      let fogNodesUpdated = {
+        ...curFogNodes,
+        [node.level]: {
+          ...[node.level],
+          [node.number]: { ...[node.number], capacity: newCapacity },
+        },
+      };
+      node.context().flow.set("fogNodes", fogNodesUpdated);
+
+      node.send({ ...payload, forwardTo: 0, completedBy: node.number });
     });
   }
 
@@ -43,3 +104,7 @@ module.exports = function (RED) {
 };
 
 // number_of_instructions, capacity
+
+// global_context = {
+//   levelNumber: {nodeNumber: {...node}}
+// }
