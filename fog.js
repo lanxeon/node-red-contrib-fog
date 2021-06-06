62f1ab91a0624e35801a91eafd0d02dd3db36069
lanxeon <bswapnonil@yahoo.com>
2021-06-06 15:03:28 +0530

    fixed the dynamic template issue

--- fog.js
+++ fog.js
@@ -1,14 +1,15 @@
 module.exports = function (RED) {
   function Fog(config) {
     RED.nodes.createNode(this, config);
-    const node = this;
 
     // initializing properties
-    node.number = +config.number;
-    node.capacity = +config.capacity;
-    node.IPS = +config.IPS; // Instructions per second
-    node.latency = +config.latency;
-    node.level = +config.level;
+    this.number = +config.number;
+    this.capacity = +config.capacity;
+    this.IPS = +config.IPS; // Instructions per second
+    this.latency = +config.latency;
+    this.level = +config.level;
+
+    const node = this;
 
     let nodeContext = node.context(); //node context
     let flowContext = node.context().flow; //flow context (kind of like global context)
@@ -31,15 +32,14 @@ module.exports = function (RED) {
 
     // when flow reaches here with a msg
     node.on("input", async function (msg) {
-      msg.payload = msg.payload.toLowerCase();
-
       // wait for this before performing actions
       await timeout(node.latency);
 
       node.send(msg);
     });
   }
-  RED.nodes.registerType("fog", Fog);
+
+  RED.nodes.registerType("fog-node", Fog);
 };
 
 // number_of_instructions, capacity
