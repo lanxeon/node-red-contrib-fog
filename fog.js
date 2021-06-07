969c2707320ac42bacbb92abaa32b694e1c8d1d4
lanxeon <bswapnonil@yahoo.com>
2021-06-08 01:12:04 +0530

    intermediate commit

--- fog.js
+++ fog.js
@@ -9,6 +9,8 @@ module.exports = function (RED) {
       return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
+    const modes = ["highest_capacity", "earliest_response", "equal_capacity"];
+
     // initializing properties
     this.number = +config.number;
     this.capacity = +config.capacity;
@@ -16,14 +18,25 @@ module.exports = function (RED) {
     this.latency = +config.latency;
     this.level = +config.level;
 
+    // load balancing mode
+    this.mode = config.mode;
+
+    if (!modes.includes(this.mode)) {
+      this.mode = "highest_capacity";
+    }
+
     const node = this;
 
     let nodeContext = node.context(); //node context
     let flowContext = node.context().flow; //flow context (kind of like global context)
 
+    // set node context
     nodeContext.set("capacity", node.capacity);
+    nodeContext.set("load", 0);
+    nodeContext.set("loadPercentage", 0);
     nodeContext.set("jobs", []);
 
+    // get global(flow) context
     let fogNodes = flowContext.get("fogNodes");
     let highestLevel = flowContext.get("highestLevel");
 
@@ -34,14 +47,19 @@ module.exports = function (RED) {
     // for setting up the global state of fog nodes by levels and then node number
     if (!fogNodes) {
       flowContext.set("fogNodes", {
-        [this.level]: { [node.number]: { ...node } },
+        [this.level]: {
+          [node.number]: { ...node, load: 0, loadPercentage: 0 },
+        },
       });
     } else {
       flowContext.set("fogNodes", {
         ...fogNodes,
         [this.level]: fogNodes[this.level]
-          ? { ...fogNodes[this.level], [node.number]: { ...node } }
-          : { [node.number]: { ...node } },
+          ? {
+              ...fogNodes[this.level],
+              [node.number]: { ...node, load: 0, loadPercentage: 0 },
+            }
+          : { [node.number]: { ...node, load: 0, loadPercentage: 0 } },
       });
     }
 
@@ -70,14 +88,50 @@ module.exports = function (RED) {
 
         let fogNodes = node.context().flow.get("fogNodes");
         let nextLevel = fogNodes[node.level + 1];
-        let highestCap = 0;
+        let chosenNode;
         let highestCapNode;
 
-        for (const levelNode of Object.keys(nextLevel)) {
-          if (nextLevel[levelNode].capacity > highestCap) {
-            highestCap = nextLevel[levelNode].capacity;
-            highestCapNode = levelNode;
-          }
+        let highestCap = 0;
+        let lowestPercentage = 100;
+        let lowestPredictedTime = 9999999;
+
+        switch (node.mode) {
+          case "highest_capacity":
+            for (const levelNode of Object.keys(nextLevel)) {
+              if (nextLevel[levelNode].capacity > highestCap) {
+                highestCap = nextLevel[levelNode].capacity;
+                chosenNode = highestCapNode = levelNode;
+              }
+            }
+
+            break;
+
+          case "earliest_response":
+            for (const levelNode of Object.keys(nextLevel)) {
+              if (nextLevel[levelNode].capacity > highestCap) {
+                highestCap = nextLevel[levelNode].capacity;
+                highestCapNode = levelNode;
+
+                if (nextLevel[levelNode].capacity > payload.capacity) {
+                  let predictedTime =
+                    nextLevel[levelNode].timeout +
+                    (payload.instructions / nextLevel[levelNode].IPS) * 1000;
+
+                  if (predictedTime < lowestPredictedTime) {
+                    lowestPredictedTime = predictedTime;
+                    chosenNode = levelNode;
+                  }
+                }
+              }
+            }
+            if (!chosenNode) {
+              chosenNode = highestCapNode;
+            }
+
+            break;
+
+          case "equal_capacity":
+            break;
         }
 
         node.send({
@@ -89,20 +143,53 @@ module.exports = function (RED) {
         // calculate new capacity(RAM)
         let subtractedCapacity = payload.capacity;
         let curCapacity = nodeContext.get("capacity");
+        let curLoad = nodeContext.get("load");
+        let curLoadPercentage = nodeContext.get("loadPercentage");
+
         let newCapacity = curCapacity - subtractedCapacity;
+        let newLoad = curLoad + subtractedCapacity;
+        let newLoadPercentage =
+          ((curLoad + subtractedCapacity) / node.capacity) * 100;
+
+        // update node state
         nodeContext.set("capacity", newCapacity);
+        nodeContext.set("load", newLoad);
+        nodeContext.set("loadPercentage", newLoadPercentage);
 
         // update global state
         let curFogNodes = { ...node.context().flow.get("fogNodes") };
         curFogNodes[node.level][node.number]["capacity"] = newCapacity;
+        curFogNodes[node.level][node.number]["load"] = newCapacity;
+        curFogNodes[node.level][node.number]["loadPercentage"] =
+          newLoadPercentage;
         node.context().flow.set("fogNodes", curFogNodes);
 
         // calculate elapsed time and wait for it, then set capacity back
         let elapsedTime = (payload.instructions / node.IPS) * 1000; // convert seconds to ms
         await timeout(elapsedTime);
 
-        nodeContext.set("capacity", curCapacity);
-        curFogNodes[node.level][node.number]["capacity"] = curCapacity;
+        // calculate new capacity(RAM)
+        subtractedCapacity = payload.capacity;
+        curCapacity = nodeContext.get("capacity");
+        curLoad = nodeContext.get("load");
+        curLoadPercentage = nodeContext.get("loadPercentage");
+
+        newCapacity = curCapacity + subtractedCapacity;
+        newLoad = curLoad - subtractedCapacity;
+        newLoadPercentage =
+          ((curLoad - subtractedCapacity) / node.capacity) * 100;
+
+        // update node state
+        nodeContext.set("capacity", newCapacity);
+        nodeContext.set("load", newLoad);
+        nodeContext.set("loadPercentage", newLoadPercentage);
+
+        // update global state
+        let curFogNodes = { ...node.context().flow.get("fogNodes") };
+        curFogNodes[node.level][node.number]["capacity"] = newCapacity;
+        curFogNodes[node.level][node.number]["load"] = newCapacity;
+        curFogNodes[node.level][node.number]["loadPercentage"] =
+          newLoadPercentage;
         node.context().flow.set("fogNodes", curFogNodes);
 
         payload.timeout += elapsedTime;
